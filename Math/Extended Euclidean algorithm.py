670e1e251746f2afed5e7c3ff9e1162db31c23c6
Misaki <2714616793@qq.com>
2020-07-02 20:16:58 +0800

    Update Extended Euclidean algorithm.py

--- Math/Extended Euclidean algorithm.py
+++ Math/Extended Euclidean algorithm.py
@@ -12,3 +12,5 @@ def ext_euclid(a, b):
             old_s,s=s,old_s-q*s
             old_t,t=t,old_t-q*t
     return old_s, old_t, old_r
+
+# https://zh.wikipedia.org/wiki/%E6%89%A9%E5%B1%95%E6%AC%A7%E5%87%A0%E9%87%8C%E5%BE%97%E7%AE%97%E6%B3%95#算法和举例
