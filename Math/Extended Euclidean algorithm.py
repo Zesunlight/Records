81367258eea77ad46980653bbdab5735f79a5cd9
Misaki <2714616793@qq.com>
2020-07-02 20:16:12 +0800

    Create Extended Euclidean algorithm.py

---
+++ Math/Extended Euclidean algorithm.py
@@ -0,0 +1,14 @@
+# use matrix to calculate gcd
+def ext_euclid(a, b):
+    old_s,s=1,0
+    old_t,t=0,1
+    old_r,r=a,b
+    if b == 0:
+        return 1, 0, a
+    else:
+        while(r!=0):
+            q=old_r//r
+            old_r,r=r,old_r-q*r
+            old_s,s=s,old_s-q*s
+            old_t,t=t,old_t-q*t
+    return old_s, old_t, old_r
